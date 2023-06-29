482faf715e4a7af1d0224ece69b25e1bf15d1b0a
achyuth1345 <119335921+achyuth1345@users.noreply.github.com>
2023-06-29 13:42:26 +0530

    Update test.js

--- Config/test.js
+++ Config/test.js
@@ -1,1 +1,13 @@
+import{ sleep } from 'k6';
+import http from 'k6/http';
 
+export let options = {
+    duration : '30s',
+    vus : 5,
+};
+// const Url=variables.default.AssetTypeGET_baseUrl;
+// const queryparams=variables.default.AssetTypeGET_queryparams;
+export default function() {
+    // const response=http.get('https://rego-dev-api.dovertech.co.in/assetmanagement/api/v1/assettype',queryparams);
+    console.log('status Code:${response.status}');
+  }
