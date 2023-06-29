a305437f43d8fdb5300b1300c7d5a94282e76b56
achyuth <pachyuth@dovercorp.com>
2023-06-29 11:41:39 +0530

    script first commit

---
+++ Config/AssetTypeConfig.js
@@ -0,0 +1,12 @@
+
+const variables=
+{
+AssetTypeGET_baseUrl:'https://rego-dev-api.dovertech.co.in/assetmanagement/api/v1/assettype',
+AssetTypeGET_queryparams:{
+    assetTenantIdentifier:"rego",
+    status:"All"
+}
+}
+
+
+export default variables;
---
+++ TestCases/AssetTypeAPI/AssetTypeGET.js
@@ -0,0 +1,18 @@
+import{ sleep } from 'k6';
+import http from 'k6/http';
+import * as variables from '../../Config/AssetTypeConfig.js';
+
+export let options = {
+    duration : '30s',
+    vus : 5,
+};
+const Url=variables.default.AssetTypeGET_baseUrl;
+const queryparams=variables.default.AssetTypeGET_queryparams;
+export default function() {
+    
+
+
+
+    const response=http.get('https://rego-dev-api.dovertech.co.in/assetmanagement/api/v1/assettype',queryparams);
+    console.log('status Code:${response.status}');
+  }
